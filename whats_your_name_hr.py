eb1a7cdec24823f9968ed9c83d0337bfe2b7a546
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-07 23:11:03 +0530

    Add files via upload

---
+++ whats_your_name_hr.py
@@ -0,0 +1,10 @@
+"""
+printing What's your Name in Python HackerRank  
+"""
+
+def print_full_name(first, last):
+    
+    print("Hello {} {}! You just delved into python.".format(first,last))
+first_name = input()
+last_name = input()
+print_full_name(first_name, last_name)
